50ddfcdf0946a1fe1a89d964cf3fdd93d67450f1
kobiam <kobi@spinningops.com>
2024-08-23 18:59:26 +0300

    better if else statement

---
+++ python/if-else-statement/main.py
@@ -0,0 +1,11 @@
+''' better if else statement '''
+
+
+def better_if_else_statement(better):
+    ''' better if else statement '''
+    some_string = "better if else statment" if better else "regular if else statement"
+    print(some_string)
+
+
+if __name__ == "__main__":
+    better_if_else_statement(better=True)
